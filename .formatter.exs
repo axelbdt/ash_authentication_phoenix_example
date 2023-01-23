c77cb1aaf927031dea9369042b23432be3076bd7
Axel Baudot <axel.baudot@protonmail.com>
2023-01-23 18:46:03 +0100

    add auth to user resource

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:phoenix, :ash, :ash_postgres],
+  import_deps: [:phoenix, :ash, :ash_postgres, :ash_authentication],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
--- lib/ash_auth_phoenix_example/accounts/resources/user.ex
+++ lib/ash_auth_phoenix_example/accounts/resources/user.ex
@@ -1,5 +1,6 @@
 defmodule AshAuthPhoenixExample.Accounts.User do
   use Ash.Resource,
+    extensions: [AshAuthentication],
     data_layer: AshPostgres.DataLayer
 
   actions do
@@ -8,6 +9,23 @@ defmodule AshAuthPhoenixExample.Accounts.User do
 
   attributes do
     uuid_primary_key :id
+    attribute :email, :ci_string, allow_nil?: false
+    attribute :hashed_password, :string, allow_nil?: false, sensitive?: true
+  end
+
+  authentication do
+    api AshAuthPhoenixExample.Accounts
+
+    strategies do
+      password :password do
+        identity_field :email
+        hashed_password_field :hashed_password
+      end
+    end
+  end
+
+  identities do
+    identity :unique_email, [:email]
   end
 
   postgres do
